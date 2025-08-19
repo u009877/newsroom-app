58491ce33ee0d91c9f54716d83dcee65616b5653
u009877 <u0098777@gmail.com>
2025-08-19 13:50:15 +0900

    chore: remove old .js config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 import typography from '@tailwindcss/typography'
-import lineClamp from '@tailwindcss/line-clamp'
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: { extend: {} },
-  plugins: [typography, lineClamp],
+  plugins: [typography],
 }
